e8aa7192efb5af2a5669f308dbd94c506c990dab
MobHam <ehsansniper123@gmail.com>
2025-08-09 11:25:14 +0330

    sample

---
+++ HtmlReader/HtmlReader/HtmlReader/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace HtmlReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.browsefiles = new System.Windows.Forms.Button();
+            this.deletefiles = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(455, 328);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // browsefiles
+            // 
+            this.browsefiles.Location = new System.Drawing.Point(659, 313);
+            this.browsefiles.Name = "browsefiles";
+            this.browsefiles.Size = new System.Drawing.Size(129, 42);
+            this.browsefiles.TabIndex = 2;
+            this.browsefiles.Text = "Browse";
+            this.browsefiles.UseVisualStyleBackColor = true;
+            this.browsefiles.Click += new System.EventHandler(this.browsefiles_Click);
+            // 
+            // deletefiles
+            // 
+            this.deletefiles.Location = new System.Drawing.Point(504, 313);
+            this.deletefiles.Name = "deletefiles";
+            this.deletefiles.Size = new System.Drawing.Size(129, 42);
+            this.deletefiles.TabIndex = 3;
+            this.deletefiles.Text = "Delete";
+            this.deletefiles.UseVisualStyleBackColor = true;
+            this.deletefiles.Click += new System.EventHandler(this.deletefiles_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(504, 27);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(284, 260);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.deletefiles);
+            this.Controls.Add(this.browsefiles);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Allame Helli";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button browsefiles;
+        private System.Windows.Forms.Button deletefiles;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ HtmlReader/HtmlReader/HtmlReader/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace HtmlReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Welcome to html reader", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void DisplaySelectedFolder(string folderPath)
+        {
+
+            ListViewItem folderItem = new ListViewItem(folderPath);
+            listView2.Items.Add(folderItem);
+
+        }
+
+        private void browsefiles_Click(object sender, EventArgs e)
+        {
+
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFolder = folderBrowserDialog.SelectedPath;
+                    DisplaySelectedFolder(selectedFolder);
+                    LoadHtmlFiles(selectedFolder);
+                }
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deletefiles_Click(object sender, EventArgs e)
+        {
+
+            if (listView1.SelectedItems.Count > 0)
+            {
+
+                foreach (ListViewItem selectedItem in listView1.SelectedItems)
+                {
+                    listView1.Items.Remove(selectedItem);
+                }
+
+                MessageBox.Show("Selected files deleted.", "successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select one or more files.", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+
+
+        private void LoadHtmlFiles(string folderPath)
+        {
+
+            try
+            {
+                var allFiles = Directory.EnumerateFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
+                               .Where(file => file.EndsWith(".html", StringComparison.OrdinalIgnoreCase) ||
+                                             file.EndsWith(".htm", StringComparison.OrdinalIgnoreCase))
+                               .ToList();
+
+                foreach (string file in allFiles)
+                {
+                    ListViewItem item = new ListViewItem(Path.GetFileName(file));
+                    item.SubItems.Add(file);
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading files: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "HTML Files|*.html;*.htm";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "Select HTML Files";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    ListViewItem item = new ListViewItem(Path.GetFileName(file));
+                    item.SubItems.Add(file);
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+    }
+}
