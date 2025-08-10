e2f8d56eb25c83140912ad8c5c5a179b43919ff6
MobHam <ehsansniper123@gmail.com>
2025-08-10 12:14:54 +0330

    fix the delete folder and fix the show all tmand html files in on folder or a folder that is in another folder
    finish

--- HtmlReader/HtmlReader/HtmlReader/Form1.Designer.cs
+++ HtmlReader/HtmlReader/HtmlReader/Form1.Designer.cs
@@ -35,16 +35,13 @@
             this.listView2 = new System.Windows.Forms.ListView();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 27);
+            this.listView1.Location = new System.Drawing.Point(12, 45);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(455, 328);
             this.listView1.TabIndex = 0;
@@ -53,84 +50,66 @@
             // 
             // browsefiles
             // 
-            this.browsefiles.Location = new System.Drawing.Point(659, 313);
+            this.browsefiles.Location = new System.Drawing.Point(659, 331);
             this.browsefiles.Name = "browsefiles";
             this.browsefiles.Size = new System.Drawing.Size(129, 42);
             this.browsefiles.TabIndex = 2;
-            this.browsefiles.Text = "Browse";
+            this.browsefiles.Text = "انتخاب فولدر";
             this.browsefiles.UseVisualStyleBackColor = true;
             this.browsefiles.Click += new System.EventHandler(this.browsefiles_Click);
             // 
             // deletefiles
             // 
-            this.deletefiles.Location = new System.Drawing.Point(504, 313);
+            this.deletefiles.Location = new System.Drawing.Point(473, 45);
             this.deletefiles.Name = "deletefiles";
-            this.deletefiles.Size = new System.Drawing.Size(129, 42);
+            this.deletefiles.Size = new System.Drawing.Size(99, 42);
             this.deletefiles.TabIndex = 3;
-            this.deletefiles.Text = "Delete";
+            this.deletefiles.Text = "حذف فایل ";
             this.deletefiles.UseVisualStyleBackColor = true;
             this.deletefiles.Click += new System.EventHandler(this.deletefiles_Click);
             // 
             // listView2
             // 
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(504, 27);
+            this.listView2.Location = new System.Drawing.Point(578, 45);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(284, 260);
+            this.listView2.Size = new System.Drawing.Size(210, 261);
             this.listView2.TabIndex = 4;
             this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // menuStrip1
+            // button1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 5;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "&File";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openToolStripMenuItem.Text = "open";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            this.button1.Location = new System.Drawing.Point(578, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "حذف فولدر";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.deletefiles);
             this.Controls.Add(this.browsefiles);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Allame Helli";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Text = "علامه حلی";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -142,9 +121,7 @@
         private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HtmlReader/HtmlReader/HtmlReader/Form1.cs
+++ HtmlReader/HtmlReader/HtmlReader/Form1.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace HtmlReader
@@ -16,8 +17,33 @@ namespace HtmlReader
     {
         public Form1()
         {
+
             InitializeComponent();
-            MessageBox.Show("Welcome to html reader", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            this.Text = "خواننده فایل‌های HTML";
+
+            this.Font = new Font("Tahoma", 9, FontStyle.Regular);
+
+            try
+            {
+                this.Font = new Font("B Nazanin", 11, FontStyle.Regular);
+            }
+            catch
+            {
+                this.Font = new Font("Tahoma", 9, FontStyle.Regular);
+            }
+
+            //this.RightToLeft = RightToLeft.Yes; 
+            //this.RightToLeftLayout = true; 
+
+            listView1.RightToLeft = RightToLeft.Yes;
+            listView1.RightToLeftLayout = true;
+
+            browsefiles.RightToLeft = RightToLeft.Yes;
+            deletefiles.RightToLeft = RightToLeft.Yes;
+
+            MessageBox.Show(" به Html Reader خوش آمدید ", "خوش آمدید", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
 
         private void DisplaySelectedFolder(string folderPath)
@@ -33,6 +59,10 @@ namespace HtmlReader
 
             using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
             {
+
+                folderBrowserDialog.Description = "لطفاً پوشه مورد نظر را انتخاب کنید";
+                folderBrowserDialog1.ShowNewFolderButton = false;
+
                 if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
                     string selectedFolder = folderBrowserDialog.SelectedPath;
@@ -59,11 +89,11 @@ namespace HtmlReader
                     listView1.Items.Remove(selectedItem);
                 }
 
-                MessageBox.Show("Selected files deleted.", "successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("فایل های انتخاب شده حذف شدند", "موفق", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Please select one or more files.", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("لطفا یک یا چند فایل رو انتخاب کنید", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
         }
@@ -75,10 +105,13 @@ namespace HtmlReader
 
             try
             {
-                var allFiles = Directory.EnumerateFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
-                               .Where(file => file.EndsWith(".html", StringComparison.OrdinalIgnoreCase) ||
-                                             file.EndsWith(".htm", StringComparison.OrdinalIgnoreCase))
-                               .ToList();
+
+                var allFiles = Directory.EnumerateFiles(folderPath, "*.*", SearchOption.AllDirectories)
+                                   .Where(file => file.EndsWith(".html", StringComparison.OrdinalIgnoreCase) ||
+                                                 file.EndsWith(".htm", StringComparison.OrdinalIgnoreCase))
+                                   .ToList();
+
+
 
                 foreach (string file in allFiles)
                 {
@@ -89,25 +122,89 @@ namespace HtmlReader
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error loading files: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"خطا در بارگیری فایل ها: {ex.Message}", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
         }
 
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            openFileDialog1.Filter = "HTML Files|*.html;*.htm";
-            openFileDialog1.Multiselect = true;
-            openFileDialog1.Title = "Select HTML Files";
 
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            //openFileDialog1.Filter = "HTML Files|*.html;*.htm";
+            //openFileDialog1.Multiselect = true;
+            //openFileDialog1.Title = "Select HTML Files";
+
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+
+            //    foreach (string file in openFileDialog1.FileNames)
+            //    {
+            //        ListViewItem item = new ListViewItem(Path.GetFileName(file));
+            //        item.SubItems.Add(file);
+            //        listView1.Items.Add(item);
+            //    }
+            //}
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (listView2.SelectedItems.Count > 0)
             {
+                DialogResult result = MessageBox.Show("آیا مطمئن هستید که می‌خواهید این پوشه و تمام محتویات آن را حذف کنید؟",
+                                                    "تأیید حذف",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Warning);
 
-                foreach (string file in openFileDialog1.FileNames)
+                if (result == DialogResult.Yes)
                 {
-                    ListViewItem item = new ListViewItem(Path.GetFileName(file));
-                    item.SubItems.Add(file);
-                    listView1.Items.Add(item);
+                    foreach (ListViewItem selectedItem in listView2.SelectedItems)
+                    {
+                        DeleteFolderFromListView(selectedItem);
+                    }
                 }
+            }
+            else
+            {
+                MessageBox.Show("لطفاً یک پوشه را انتخاب کنید.", "هشدار",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }        
+
+        private void DeleteFolderFromListView(ListViewItem item)
+        {
+            try
+            {
+                string folderPath = item.Text;
+
+
+                listView2.Items.Remove(item);
+
+                foreach (ListViewItem fileItem in listView1.Items.Cast<ListViewItem>().ToList())
+                {
+                    if (fileItem.SubItems[1].Text.StartsWith(folderPath))
+                    {
+                        listView1.Items.Remove(fileItem);
+                    }
+                }
+
+                MessageBox.Show("پوشه و تمام محتویات آن با موفقیت حذف شد.", "موفقیت",
+                               MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در حذف پوشه: {ex.Message}", "خطا",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
